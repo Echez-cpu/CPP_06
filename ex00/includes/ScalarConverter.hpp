5d51662e7e0b2bcd3c2c6a62f3cb8a86eb00a07c
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-12 16:03:16 +0100

    Create ScalarConverter.hpp

---
+++ ex00/includes/ScalarConverter.hpp
@@ -0,0 +1,30 @@
+#ifndef  SCALARCONVERTER_HPP
+#define  SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <climits>
+#include <limits.h>
+#include <float.h>
+#include <cstdlib>
+
+typedef std::string str
+
+
+class ScalarConverter {
+
+	private:
+		ScalarConverter();
+		ScalarConverter(ScalarConverter const &source);
+		~ScalarConverter();
+
+
+ScalarConverter &	operator=(ScalarConverter const &original_copy);
+
+public:
+		static void	convert(str &lit);
+
+};
+
+#endif
