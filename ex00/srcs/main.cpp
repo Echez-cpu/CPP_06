762d700a279f127bc566c8049407497785dd84e9
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-13 15:34:26 +0100

    Create main.cpp

---
+++ ex00/srcs/main.cpp
@@ -0,0 +1,10 @@
+int main(int argc, char** argv)
+{
+    if (argc != 2) {
+        std::cerr << "Usage: ./convert <String literal>" << std::endl;
+        return 1;
+    }
+
+    ScalarConverter::convert(argv[1]);
+    return 0;
+}
