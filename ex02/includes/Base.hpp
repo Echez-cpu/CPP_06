8323a244feacf31348cdc21367f5050d5e086c99
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-14 18:16:05 +0100

    Create Base.hpp

---
+++ ex02/includes/Base.hpp
@@ -0,0 +1,16 @@
+#ifndef  BASE_HPP
+#define  BASE_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <ctime>
+#include <cstdlib>
+
+
+class Base {
+	public:
+		virtual ~Base();
+};
+
+#endif
