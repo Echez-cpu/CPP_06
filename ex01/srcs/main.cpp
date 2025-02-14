b7de62bbc425db00265e4b4e4689ed7fe373b59e
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-02-14 15:40:29 +0100

    Create main.cpp

---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,22 @@
+#include "../includes/Serializer.hpp"
+
+int main() {
+    Data originalData;
+    originalData.name = "John Doe";
+    originalData.age = 30;
+
+    
+    uintptr_t raw = Serializer::serialize(&originalData);
+    std::cout << "Serialized value: " << raw << std::endl;
+
+    Data* deserializedData = Serializer::deserialize(raw);
+
+    std::cout << "Deserialized Data -> Name: " << deserializedData->name << ", Age: " << deserializedData->age << std::endl;
+
+    if (deserializedData == &originalData)
+        std::cout << "Serialization and deserialization successful!" << std::endl;
+    else
+        std::cout << "Serialization failed!" << std::endl;
+
+    return 0;
+}
